53e1831768d173db84c96c726ac1c9fb4179a4de
ShotaNinomiya <shota.ninomiya@aimnext.com>
2024-11-29 18:49:17 +0900

    config修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WeatherApp/', // リポジトリ名をベースパスに設定
+  base: "./", // リポジトリ名をベースパスに設定
   build: {
     sourcemap: false, // ソースマップを無効化
   },
-})
+});
